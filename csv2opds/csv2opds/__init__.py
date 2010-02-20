e2b3f43b05391e686640e731ae30b7e71e76c6c5
Keith Fahlgren <abdelazer@gmail.com>
2010-02-20 09:35:52 -0800

    A CSV file should be able to be opened ... ok An OPDS Catalog should be outputted as a set of Atom feeds ... ok An OPDS Catalog should be outputted as a set of valid Atom feeds ... ok An OPDS Catalog should be outputted as well-formed XML ... ok An OPDS Catalog should be able to be outputted to a directory ... ok

--- csv2opds/csv2opds/__init__.py
+++ csv2opds/csv2opds/__init__.py
@@ -7,6 +7,29 @@ Created by Keith Fahlgren on Sat Feb 20 08:02:24 PST 2010
 Copyright (c) 2010 Threepress Consulting Inc. All rights reserved.
 """
 
+import logging
+import os
+import uuid
+
+import genshi.template 
+
+log = logging.getLogger(__name__)
+
+UUID_KEY = uuid.UUID('31cce9f0-ed0f-4ce0-a4e5-da331ed7341e')
+
 class Opds(object):
-    def __init__(self, csv_file):
-        pass
+
+    def __init__(self, csv_file, author):
+        template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
+        self.template_loader = genshi.template.TemplateLoader([template_dir])
+        self.author = author
+        self.uuid_master = uuid.uuid3(UUID_KEY, self.author)
+
+    def output_catalog(self, output_dir):
+        catalog_root_fn = os.path.join(output_dir, 'opds.xml')
+        f = open(catalog_root_fn, 'w')
+        tmpl = self.template_loader.load('root.xml')
+        root_urn = 'urn:uuid:' + str(uuid.uuid3(self.uuid_master, 'root.xml'))
+        output = tmpl.generate(feed_author=self.author, atom_id=root_urn)
+        f.write(output.render())
+        f.close()
--- csv2opds/csv2opds/tests/test_opds.py
+++ csv2opds/csv2opds/tests/test_opds.py
@@ -7,18 +7,87 @@ Created by Keith Fahlgren on Sat Feb 20 07:52:48 PST 2010
 Copyright (c) 2010 Threepress Consulting Inc. All rights reserved.
 """
 
+
+import csv
+import glob
+import logging
 import os
+import shutil
+import tempfile
+import uuid
+
+from lxml import etree
+from nose.tools import *
+
 import csv2opds
 
-from nose.tools import *
+log = logging.getLogger(__name__)
 
 class TestOpds(object):
     def setup(self):
+        self.author = str(uuid.uuid4())
         self.filedir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'files'))
+        self.tempdir = tempfile.mkdtemp()
+        self.simple_csv_fn = os.path.join(self.filedir, 'simple.csv')
+        self._write_simple_csv(self.simple_csv_fn)
+        self.opds = csv2opds.Opds(self.simple_csv_fn, self.author)
+        atom_rng_fn = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'schemas', 'atom.rng'))
+        self.atom_validator = etree.RelaxNG(etree.parse(atom_rng_fn))
 
     def test_init(self):
         """A CSV file should be able to be opened"""
-        file = os.path.join(self.filedir, 'simple.csv')
-        opds = csv2opds.Opds(file)
+        opds = csv2opds.Opds(self.simple_csv_fn, self.author)
         assert opds
+
+    def test_output_something(self):
+        """An OPDS Catalog should be able to be outputted to a directory"""
+        self.opds.output_catalog(self.tempdir)
+        catalog_files = glob.glob(self.tempdir + '/*.xml')
+        assert len(catalog_files) > 0
+
+
+    def test_output_is_xml(self):
+        """An OPDS Catalog should be outputted as well-formed XML"""
+        self.opds.output_catalog(self.tempdir)
+        catalog_files = glob.glob(self.tempdir + '/*.xml')
+        for f in catalog_files:
+            xml = etree.parse(f)
+            assert xml
+
+    def test_output_is_a_feed(self):
+        """An OPDS Catalog should be outputted as a set of Atom feeds"""
+        self.opds.output_catalog(self.tempdir)
+        catalog_files = glob.glob(self.tempdir + '/*.xml')
+        for f in catalog_files:
+            xml = etree.parse(f)
+            xml_root_tag = xml.getroot().tag
+            expected = '{http://www.w3.org/2005/Atom}feed'
+            assert_equal(expected, xml_root_tag)
+
+
+    def test_output_is_valid_atom(self):
+        """An OPDS Catalog should be outputted as a set of valid Atom feeds"""
+        self.opds.output_catalog(self.tempdir)
+        catalog_files = glob.glob(self.tempdir + '/*.xml')
+        for f in catalog_files:
+            xml = etree.parse(f)
+            try:
+                assert self.atom_validator.validate(xml)
+            except AssertionError:
+                error_log = self.atom_validator.error_log
+                log.warn('Validation errors:\n%s' % error_log)
+                raise
+
+    def teardown(self):
+        shutil.rmtree(self.tempdir)
+
+    def _write_simple_csv(self, csv_fn):
+        writer = csv.writer(open(csv_fn, "wb")) 
+        headers = ['ISBN', 'Title', 'Authors (all first last, joined by commas, no ands)', 'Publication Date (YYYY-MM-DD)', 'Publisher', 'Price (empty for free)', 'Currency', 'ePub URL', 'PDF URL', 'Mobi URL', 'Cover Thumbnail URL', 'Language', 'Description (text, less than 2000 chars)', 'Rights', 'Publisher ID', 'Rank', 'On Featured List? (blank for no)']
+        row1 = ['978-0-00-000000-0', 'A Year of Breakfasts', 'Liza Daly', '2009-09-23', 'Threepress', '12.50', 'USD', 'http://threepress.org/examples/opds/downloads/1.epub', '', '', 'http://threepress.org/examples/opds/covers/1.thumb.jpg', 'en', "Today was waffle day! I had a waffle and decaf coffee and orange juice. Normally I have one and a half waffles, but today there was less waffle batter than usual and I wasn't very hungry because I ate a lot of food last night so I only had one waffle and that was fine.", 'Copyright \xa9 2009 Liza Daly', 'http://threepress.org/examples/opds/identifiers/1', '2', '']
+        row2 = ['9780000000010', 'Collaborative Works Rock', 'Keith Fahlgren, Liza Daly, Elizabeth Fahlgren', '2007-11-01', 'Threepress', '', '', 'http://threepress.org/examples/opds/downloads/2.epub', 'http://threepress.org/examples/opds/downloads/2.pdf', '', '', 'en', 'A gripping thriller.', '', 'http://threepress.org/examples/opds/identifiers/2', '1', 'y']
+        row3 = ['', 'R\x8edacteurix, une Histoire', 'Liz\x87 D\x89ly', '1899', 'Troispresse', '9.99', 'EUR', 'http://threepress.org/examples/opds/downloads/3.epub', 'http://threepress.org/examples/opds/downloads/3.pdf', 'http://threepress.org/examples/opds/downloads/3.mobi', 'http://threepress.org/examples/opds/covers/3.thumb.png', 'fr', 'http://threepress.org/examples/opds/covers/3.thumb.png', '', 'http://threepress.org/examples/opds/identifiers/3', '', '']
+
+        writer.writerows(headers + row1 + row2 + row3)
+
 
