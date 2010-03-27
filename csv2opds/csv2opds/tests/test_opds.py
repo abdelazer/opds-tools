676aa27caf6cc2c2d94783aecd402764dc3c9fdb
Keith Fahlgren <abdelazer@gmail.com>
2010-03-26 18:45:36 -0700

    Ensure that a sparsely populated CSV may also be used as input

--- csv2opds/csv2opds/tests/test_opds.py
+++ csv2opds/csv2opds/tests/test_opds.py
@@ -29,10 +29,10 @@ class TestOpds(object):
         self.author = str(uuid.uuid4())
         self.filedir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'files'))
         self.tempdir = tempfile.mkdtemp()
-        self.simple_csv_fn = os.path.join(self.filedir, 'simple.csv')
-        self._write_simple_csv(self.simple_csv_fn)
+        self.rich_csv_fn = os.path.join(self.filedir, 'rich.csv')
+        self._write_rich_csv(self.rich_csv_fn)
         self.catalog_title = str(uuid.uuid4())
-        self.opds = csv2opds.Opds(self.simple_csv_fn, self.author, title=self.catalog_title)
+        self.opds = csv2opds.Opds(self.rich_csv_fn, self.author, title=self.catalog_title)
         atom_rng_fn = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'schemas', 'atom.rng'))
         self.atom_validator = etree.RelaxNG(etree.parse(atom_rng_fn))
         self.opds.output_catalog(self.tempdir)
@@ -44,12 +44,22 @@ class TestOpds(object):
 
     def test_init(self):
         """A CSV file should be able to be opened"""
-        opds = csv2opds.Opds(self.simple_csv_fn, self.author)
+        opds = csv2opds.Opds(self.rich_csv_fn, self.author)
         assert opds
 
     def test_output_something(self):
         """An OPDS Catalog should be able to be outputted to a directory"""
         assert len(self.catalog_files) > 0
+
+    def test_sparse_input(self):
+        """An OPDS Catalog should be able to be created from a very minimal CSV file"""
+        tempdir = tempfile.mkdtemp()
+        csv_fn = os.path.join(self.filedir, 'sparse.csv')
+        self._write_sparse_csv(csv_fn)
+        opds = csv2opds.Opds(csv_fn, self.author)
+        opds.output_catalog(tempdir)
+        catalog_files = glob.glob(tempdir + '/*.xml')
+        assert len(catalog_files) > 0
 
     def test_output_is_xml(self):
         """An OPDS Catalog should be outputted as well-formed XML"""
@@ -115,12 +125,24 @@ class TestOpds(object):
             catalogs_as_xml.append(xml)
         return catalogs_as_xml
 
-    def _write_simple_csv(self, csv_fn):
+    def _write_sparse_csv(self, csv_fn):
         writer = csv.writer(open(csv_fn, "wb")) 
-        headers = ('ISBN', 'Title', 'Authors (all first last, joined by commas, no ands)', 'Publication Date (YYYY-MM-DD)', 'Publisher', 'Price (empty for free)', 'Currency', 'ePub URL', 'PDF URL', 'Mobi URL', 'Cover Thumbnail URL', 'Language', 'Description (text, less than 2000 chars)', 'Rights', 'Publisher ID', 'Rank', 'On Featured List? (blank for no)')
-        row1 = ('978-0-00-000000-0', 'A Year of Breakfasts', 'Liza Daly', '2009-09-23', 'Threepress', '12.50', 'USD', 'http://threepress.org/examples/opds/downloads/1.epub', '', '', 'http://threepress.org/examples/opds/covers/1.thumb.jpg', 'en', "Today was waffle day! I had a waffle and decaf coffee and orange juice. Normally I have one and a half waffles, but today there was less waffle batter than usual and I wasn't very hungry because I ate a lot of food last night so I only had one waffle and that was fine.", 'Copyright \xa9 2009 Liza Daly', 'http://threepress.org/examples/opds/identifiers/1', '2', '')
-        row2 = ('9780000000010', 'Collaborative Works Rock', 'Keith Fahlgren, Liza Daly, Elizabeth Fahlgren', '2007-11-01', 'Threepress', '', '', 'http://threepress.org/examples/opds/downloads/2.epub', 'http://threepress.org/examples/opds/downloads/2.pdf', '', '', 'en', 'A gripping thriller.', '', 'http://threepress.org/examples/opds/identifiers/2', '1', 'y')
-        row3 = ('', 'Redacteurix, une Histoire', 'Liza Daly', '1899', 'Troispresse', '9.99', 'EUR', 'http://threepress.org/examples/opds/downloads/3.epub', 'http://threepress.org/examples/opds/downloads/3.pdf', 'http://threepress.org/examples/opds/downloads/3.mobi', 'http://threepress.org/examples/opds/covers/3.thumb.png', 'fr', 'A mystery of Gallic intensity', '', 'http://threepress.org/examples/opds/identifiers/3', '', '')
+        headers = ('ISBN',              'Title',                     'Authors (all first last, joined by commas, no ands)', 'Publication Date (YYYY-MM-DD)', 'Publisher',   'Price (empty for free)', 'Currency', 'ePub URL',                                             'PDF URL', 'Mobi URL', 'Cover Thumbnail URL', 'Language', 'Description (text, less than 2000 chars)', 'Rights', 'Publisher ID', 'Rank', 'On Featured List? (blank for no)')
+        row1    = ('978-0-00-000000-0', 'A Year of Breakfasts',      'Liza Daly',                                           '2009-09-23',                    'Threepress',  '',                       '',         'http://threepress.org/examples/opds/downloads/1.epub', '',        '',         '',                    '',         "Today was waffle day!",                    '',       '',             '',     '')
+        row2    = ('9780000000010',     'Collaborative Works Rock',  'Keith Fahlgren, Liza Daly, Elizabeth Fahlgren',       '2007-11-01',                    'Threepress',  '',                       '',         'http://threepress.org/examples/opds/downloads/2.epub', '',        '',         '',                    '',         'A gripping thriller.',                     '',       '',             '',     '')
+        row3    = ('',                  'Redacteurix, une Histoire', 'Liza Daly',                                           '1899',                          'Troispresse', '',                       '',         'http://threepress.org/examples/opds/downloads/3.epub', '',        '',         '',                    '',         'A mystery of Gallic intensity',            '',       '',             '',     '')
+
+        writer.writerow(headers)
+        writer.writerow(row1)
+        writer.writerow(row2)
+        writer.writerow(row3)
+
+    def _write_rich_csv(self, csv_fn):
+        writer = csv.writer(open(csv_fn, "wb")) 
+        headers = ('ISBN',              'Title',                     'Authors (all first last, joined by commas, no ands)', 'Publication Date (YYYY-MM-DD)', 'Publisher',   'Price (empty for free)', 'Currency', 'ePub URL',                                             'PDF URL',                                             'Mobi URL',                                             'Cover Thumbnail URL',                                    'Language', 'Description (text, less than 2000 chars)',                                                                                                                                                                                                                                      'Rights',                        'Publisher ID',                                      'Rank', 'On Featured List? (blank for no)')
+        row1    = ('978-0-00-000000-0', 'A Year of Breakfasts',      'Liza Daly',                                           '2009-09-23',                    'Threepress',  '12.50',                  'USD',      'http://threepress.org/examples/opds/downloads/1.epub', '',                                                    '',                                                     '',                                                       '',         "Today was waffle day! I had a waffle and decaf coffee and orange juice. Normally I have one and a half waffles, but today there was less waffle batter than usual and I wasn't very hungry because I ate a lot of food last night so I only had one waffle and that was fine.", 'Copyright \xa9 2009 Liza Daly', 'http://threepress.org/examples/opds/identifiers/1', '2',    '')
+        row2    = ('9780000000010',     'Collaborative Works Rock',  'Keith Fahlgren, Liza Daly, Elizabeth Fahlgren',       '2007-11-01',                    'Threepress',  '',                       '',         '',                                                     'http://threepress.org/examples/opds/downloads/2.pdf', '',                                                     '',                                                       'en',       'A gripping thriller.',                                                                                                                                                                                                                                                          '',                              'http://threepress.org/examples/opds/identifiers/2', '1',    'y')
+        row3    = ('',                  'Redacteurix, une Histoire', 'Liza Daly',                                           '1899',                          'Troispresse', '9.99',                   'EUR',      'http://threepress.org/examples/opds/downloads/3.epub', 'http://threepress.org/examples/opds/downloads/3.pdf', 'http://threepress.org/examples/opds/downloads/3.mobi', 'http://threepress.org/examples/opds/covers/3.thumb.png', 'fr',       'A mystery of Gallic intensity',                                                                                                                                                                                                                                                 '',                              'http://threepress.org/examples/opds/identifiers/3', '',     'y')
         # FIXME: Unicode
         #row3 = ('', u'R\x8edacteurix, une Histoire', u'Liz\x87 D\x89ly', '1899', 'Troispresse', '9.99', 'EUR', 'http://threepress.org/examples/opds/downloads/3.epub', 'http://threepress.org/examples/opds/downloads/3.pdf', 'http://threepress.org/examples/opds/downloads/3.mobi', 'http://threepress.org/examples/opds/covers/3.thumb.png', 'fr', 'http://threepress.org/examples/opds/covers/3.thumb.png', '', 'http://threepress.org/examples/opds/identifiers/3', '', '')
 
