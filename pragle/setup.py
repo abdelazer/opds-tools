c6592b5bc5b648af7132e23f33a914d5bb6ca124
Keith Fahlgren <abdelazer@gmail.com>
2010-06-03 15:55:42 -0700

    Initial import for Pragle

---
+++ pragle/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='pragle',
+      version=version,
+      description="Pragle is a Python library for validating OPDS Catalogs",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='opds',
+      author='Keith Fahlgren',
+      author_email='keith@threepress.org',
+      url='http://code.google.com/p/opds-tools/',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
